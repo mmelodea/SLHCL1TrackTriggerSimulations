8837036591013aeb7cc9da0a52c9bda70e894f5b
Jia Fu Low <jia.fu.low@cern.ch>
2015-11-16 20:51:31 -0600

    Dropped ntupleGenExtra from the ntuple sequence

--- NTupleTools/python/sequences_cff.py
+++ NTupleTools/python/sequences_cff.py
@@ -7,7 +7,7 @@ from SLHCL1TrackTriggerSimulations.NTupleTools.ntupleDigi_cfi import *
 from SLHCL1TrackTriggerSimulations.NTupleTools.ntupleL1TrackTrigger_cfi import *
 from SLHCL1TrackTriggerSimulations.NTupleTools.ntupleMaker_cfi import *
 
-ntupleGen += ntupleGenExtra
+#ntupleGen += ntupleGenExtra
 
 ntupleSequence = cms.Sequence(ntupleGen * ntupleSim * ntupleDigi * ntupleL1TrackTrigger * (ntupleEventInfo * ntupler))
 
